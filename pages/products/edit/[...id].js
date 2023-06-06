e6d8f67256a7438bda55c6d8ad8b940967893d39
maryam <50619466+marykhodaparast@users.noreply.github.com>
2023-06-06 16:25:29 +0330

    completing editing product

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,12 +1,26 @@
-import { useRouter } from 'next/router';
-import Layout from '/components/Layout';
+import { useRouter } from "next/router";
+import Layout from "/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProductForm from "../../../components/ProductForm";
 
 export default function EditProductPage() {
-    const router = useRouter();
-    console.log({ router });
-    return (
-        <Layout>
-            edit product form here
-       </Layout> 
-    );
+  const [productInfo, setProductInfo] = useState(null);
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get("/api/products?id=" + id).then((response) => {
+      setProductInfo(response.data);
+    });
+  }, [id]);
+  return (
+    <Layout>
+      <h1>Edit Product</h1>
+      {productInfo && <ProductForm {...productInfo} />}
+    </Layout>
+  );
 }
