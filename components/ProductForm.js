e6a9091d6f6bf75351bc8c9f7971c060cf4e170c
maryam <50619466+marykhodaparast@users.noreply.github.com>
2023-06-08 15:17:30 +0330

    try adding uploading files feature for each product,but not completed yet

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -30,6 +30,19 @@ export default function ProductForm({
     router.push("/products");
   }
 
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      //files.forEach(file => data.append('file', file));
+      const res = await axios.post('/api/upload', data)
+      console.log(res.data);
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -41,9 +54,29 @@ export default function ProductForm({
       />
       <label>Photos</label>
       <div className="mb-2">
-        {!images?.length && (
-          <div>No photos in this product</div>
-        )}
+        <label
+          className="w-24 h-24 text-center flex flex-col cursor-pointer
+        items-center justify-center text-sm gap-1 text-gray-500 rounded-lg
+        bg-gray-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" className="hidden" onChange={uploadImages} />
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
       </div>
       <label>Description</label>
       <textarea
