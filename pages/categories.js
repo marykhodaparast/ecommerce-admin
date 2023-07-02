9083dcdfb50264d8f38f177a6619d927f99ff449
maryam <50619466+marykhodaparast@users.noreply.github.com>
2023-07-02 18:11:36 +0330

    work on saving category but it failed... should check later....

--- pages/categories.js
+++ pages/categories.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import axios from "axios";
+import Layout from "/components/Layout";
+
 export default function CategoriesPage() {
-    
+  const [name, setName] = useState("");
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
+  }
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>New category name</label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          className="mb-0"
+          type="text"
+          placeholder={"Category name"}
+          onChange={(ev) => setName(ev.target.value)}
+          value={name}
+        />
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
+      </form>
+    </Layout>
+  );
 }
