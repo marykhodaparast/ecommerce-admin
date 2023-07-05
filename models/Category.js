55e0f7fa0e1d129e97e65b7095951eff1391274a
maryam <50619466+marykhodaparast@users.noreply.github.com>
2023-07-05 16:16:15 +0330

    fix issue of not saving categories.... add parent to category model and show categories with their name and parent id in categories page

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,8 @@
-import { Schema, model, models } from "mongoose";
+import mongoose, { models, model, Schema } from 'mongoose'
 
 const CategorySchema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    parent:{type:mongoose.Types.ObjectId}
 });
 
 export const Category = models?.Category || model('Category', CategorySchema);
--- pages/categories.js
+++ pages/categories.js
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Layout from "/components/Layout";
 
-export default function CategoriesPage() {
-  const [name, setName] = useState("");
+export default function Categories() {
+  const [name, setName] = useState('');
+  const [parentCategory, setParentCategory] = useState('');
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
+    axios.get('/api/categories').then(result => {
+      setCategories(result.data);
+    });
+  }
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post("/api/categories", { name });
-    setName("");
+    await axios.post('/api/categories', {name, parentCategory});
+    setName('');
+    fetchCategories();
   }
   return (
     <Layout>
@@ -21,10 +32,41 @@ export default function CategoriesPage() {
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+        <select
+          className="mb-0"
+          onChange={ev => setParentCategory(ev.target.value)}
+          value={parentCategory}>
+          <option value="">No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <>
+                <option value={category._id}>{category.name}</option>
+              </>
+            ))}
+        </select>
         <button type="submit" className="btn-primary py-1">
           Save
         </button>
       </form>
+      <table className="basic mt-2">
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent Category</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <>
+                <tr>
+                  <td>{category.name}</td>
+                  <td>{category.parent}</td>
+                </tr>
+              </>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
