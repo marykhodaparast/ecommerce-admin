8898990960ab3d82042ce4af34cfeb5d1c22eb33
maryam <50619466+marykhodaparast@users.noreply.github.com>
2023-07-09 17:25:01 +0330

    add category to product add form and test, it worked fine

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Spinner from "./Spinner";
@@ -13,14 +13,21 @@ export default function ProductForm({
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
+  const [category, setCategory] = useState('');
   const [price, setPrice] = useState(existingPrice || "");
   const [images, setImages] = useState(existingImages || "");
   const [goToProducts, setGoToProducts] = useState(false);
   const [isUploading, setIsUploding] = useState(false);
+  const [categories, setCategories] = useState([]);
   const router = useRouter();
+  useEffect(() => {
+    axios.get('/api/categories').then(result => {
+      setCategories(result.data);
+    });
+  }, []);
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = { title, description, price, images };
+    const data = { title, description, price, images, category };
     if (_id) {
       //update
       await axios.put("/api/products", { ...data, _id });
@@ -72,7 +79,6 @@ export default function ProductForm({
     }
   }
   function updateImagesOrder(images) {
-    //console.log(images)
     setImages(images);
   }
 
@@ -85,12 +91,25 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Category</label>
+      <select
+        value={category}
+        onChange={ev => setCategory(ev.target.value)}>
+        <option value="">Uncategorized</option>
+        {categories.length > 0 &&
+          categories.map((c) => (
+            <>
+              <option value={c._id}>{c.name}</option>
+            </>
+          ))}
+      </select>
       <label>Photos</label>
       <div className="mb-2 flex flex-wrap gap-1">
         <ReactSortable
           className="flex flex-wrap gap-1"
           list={images}
-          setList={updateImagesOrder}>
+          setList={updateImagesOrder}
+        >
           {!!images?.length &&
             images.map((path) => (
               <div key={path} className="h-24">
--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
-import { model, Schema, models } from 'mongoose'
+import mongoose, { model, Schema, models } from "mongoose";
 
 const ProductsSchema = new Schema({
-    title: {type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
-    images: { type: [String]}
+  title: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true },
+  images: { type: [String] },
+  category: { type: mongoose.Types.ObjectId, ref: "Category" },
 });
 
-export const Product = models.Product || model('Product', ProductsSchema)
+export const Product = models.Product || model("Product", ProductsSchema);
